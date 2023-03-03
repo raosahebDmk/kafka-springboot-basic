3d9f7004c618e8dd3cc4e0abc2e01d9d9a89defa
raosahebDmk <raosaheb.deshmukh88@gmail.com>
2023-03-03 11:39:53 +0530

    first commit

---
+++ src/test/java/com/example/kafkaspringbootbasic/KafkaSpringbootBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaspringbootbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringbootBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
